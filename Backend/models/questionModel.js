b68baa678b14b7e0815c8fbfaa65186bd00506bd
Megan Morgan <meganmorgan@knights.ucf.edu>
2023-11-07 19:33:33 -0500

    answer choices added to question model/controller

--- Backend/models/questionModel.js
+++ Backend/models/questionModel.js
@@ -15,7 +15,20 @@ const questionSchema = new Schema({
     module: {
         type: Number,
         required: true
+    },
+    option1: {
+        type: String,
+        required: true
+    },
+    option2: {
+        type: String,
+        required: true
+    },
+    option3: {
+        type: String,
+        required: true
     }
+
 })
 
 module.exports = mongoose.model('Question', questionSchema)
