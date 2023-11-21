fb55fa478b3dc5c85e1d2f17eaa73f78c4ac3567
Megan Morgan <meganmorgan@knights.ucf.edu>
2023-11-21 16:09:38 -0500

    changed

--- frontend/src/pages/PracticeAll.js
+++ frontend/src/pages/PracticeAll.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import RandomQuestion from '../components/RandomQuestion';
+import HintContent from '../components/HintContent';
 
 const PracticeAll = () => {
 
   const [keyProp, setKeyProp] = useState(0);
+  const [showHint, setShowHint] = useState(false);
+  const [newModuleID, setNewModuleID] = useState(null);
+  const [newPosition, setNewPosition] = useState(null);
 
   // Function to handle the "Next" button click
   const handleNextClick = () => {
     setKeyProp((prevKeyProp) => prevKeyProp + 1);
+    setShowHint(false);
+  };
+
+
+  const handleHintClick = () => {
+    setShowHint(true); // Show the hint when the hint button is clicked
   };
 
   const handleRandomValuesChange = (newModuleID, newPosition) => {
     console.log('Received random values in parent:', newModuleID, newPosition);
     // You can do further processing or state updates in the parent component if needed
   };
+
+   // useEffect to handle the initial random values
+   useEffect(() => {
+    const initialRandomModuleID = newModuleID;
+    const initialRandomPosition = newPosition;
+
+    setNewModuleID(initialRandomModuleID);
+    setNewPosition(initialRandomPosition);
+
+    // Additional logic if needed...
+
+  }, []); 
 
 
   return (
@@ -25,13 +47,25 @@ const PracticeAll = () => {
             <h4>Practice All</h4>
           </div>
 
-          <div className="random-question-content">
-            <RandomQuestion keyProp={keyProp} onRandomValuesChange={handleRandomValuesChange}/>
+          <div className="practice-div branded-shadow">
+            <div className="hint-btn-container">
+              <button className="branded-long-button branded-shadow hint-btn" onClick={handleHintClick}>Hint 💡</button>
+            </div>
+
+            <div className="random-question-content">
+              <RandomQuestion keyProp={keyProp} onRandomValuesChange={handleRandomValuesChange}/>
+            </div>
+
+            <div className="button-container-back-next">
+              <button className="branded-long-button branded-shadow next-btn" onClick={handleNextClick}>Next</button>
+            </div>
           </div>
 
-          <div className="button-container-back-next">
-            <button className="branded-long-button branded-shadow next-btn" onClick={handleNextClick}>Next</button>
-          </div>
+          {showHint && (
+            <div className="hint-box">
+              <HintContent moduleID={newModuleID} position={newPosition} />
+            </div>
+          )}
         </div>
       </div>
     </div>
