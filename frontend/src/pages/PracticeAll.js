fa4946ef6b9586b90da298981cfbfc994006a278
Megan Morgan <meganmorgan@knights.ucf.edu>
2023-11-21 15:52:08 -0500

    practice all added

--- frontend/src/pages/PracticeAll.js
+++ frontend/src/pages/PracticeAll.js
@@ -4,11 +4,36 @@ import RandomQuestion from '../components/RandomQuestion';
 
 const PracticeAll = () => {
 
+  const [keyProp, setKeyProp] = useState(0);
+
+  // Function to handle the "Next" button click
+  const handleNextClick = () => {
+    setKeyProp((prevKeyProp) => prevKeyProp + 1);
+  };
+
+  const handleRandomValuesChange = (newModuleID, newPosition) => {
+    console.log('Received random values in parent:', newModuleID, newPosition);
+    // You can do further processing or state updates in the parent component if needed
+  };
 
 
   return (
     <div>
-      <p>PracticeAll Content</p>
+      <div className="container-div">
+        <div className="practice-box">
+          <div className="path">
+            <h4>Practice All</h4>
+          </div>
+
+          <div className="random-question-content">
+            <RandomQuestion keyProp={keyProp} onRandomValuesChange={handleRandomValuesChange}/>
+          </div>
+
+          <div className="button-container-back-next">
+            <button className="branded-long-button branded-shadow next-btn" onClick={handleNextClick}>Next</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
