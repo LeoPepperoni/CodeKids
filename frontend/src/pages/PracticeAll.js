c875ed5340ec32cdbeacf9c74ab05e0d305ec109
Megan Morgan <meganmorgan@knights.ucf.edu>
2023-11-21 11:06:25 -0500

    practice all added

--- frontend/src/pages/PracticeAll.js
+++ frontend/src/pages/PracticeAll.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import RandomQuestion from '../components/RandomQuestion';
 
 const PracticeAll = () => {
+  
+  const [keyProp, setKeyProp] = useState(0);
+
+  const handleNextClick = () => {
+    setKeyProp((prevKeyProp) => prevKeyProp + 1);
+  };
+
+
   return (
     <div>
-      <h1>Welcome to the PracticeAll Page</h1>
-      <RandomQuestion />
+         <div className="container-div">
+                <div className="practice-box">
+
+                    <div className="practice-div branded-shadow">
+                        <div className="hint-btn-container">
+                            <button className="branded-long-button branded-shadow hint-btn">Hint 💡</button>
+                        </div>
+
+                        <div className="random-question-content">
+                          <RandomQuestion keyProp={keyProp} />
+                        </div>
+
+                        <div className="button-container-back-next">
+                            <button className="branded-long-button branded-shadow next-btn" onClick={handleNextClick}>Next</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </div>
   );
 };
