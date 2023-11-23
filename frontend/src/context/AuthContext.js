53749e946bf07e8ae504e59636e530614a295061
Noah Beightol <noahbeightol14@gmail.com>
2023-11-23 01:29:43 -0500

    revert auth changes

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -9,7 +9,7 @@ export const authReducer = (state, action) => {
         case 'LOGOUT':
             return { user: null }
         default:
-            return { user: null }
+            return state
     }
 }
 
