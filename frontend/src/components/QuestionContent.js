72f5d195bcb58b17b31264522d1025328c181217
Noah Beightol <noahbeightol14@gmail.com>
2023-11-28 22:04:03 -0500

    fix missing usestate import

--- frontend/src/components/QuestionContent.js
+++ frontend/src/components/QuestionContent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import useGetQuestion from '../hook/useGetQuestion';
 
 const QuestionContent = ({ moduleID, position }) => {
